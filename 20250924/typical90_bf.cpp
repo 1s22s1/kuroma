70b738f4afd168fc5801e82621c2c8a85d3ce445
1s22s1 <1s22s1@users.noreply.github.com>
2025-09-24 17:49:25 +0900

    :up:

---
+++ 20250924/typical90_bf.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define endl '\n'
+#define ll long long
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+
+void init()
+{
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+}
+
+int main()
+{
+    init();
+
+    ll N, K;
+    cin >> N >> K;
+
+    set<ll> set;
+    vector<ll> path;
+    ll stright_length, loop_length;
+
+    while (true)
+    {
+        ll current = N;
+        if (set.count(current))
+        {
+            auto target_it = find(path.begin(), path.end(), current);
+            stright_length = distance(path.begin(), target_it);
+            loop_length = path.size() - stright_length;
+
+            break;
+        }
+        else
+        {
+            path.emplace_back(current);
+            set.insert(current);
+
+            while (N > 0)
+            {
+                current += N % 10;
+                N /= 10;
+            }
+
+            N = current % 100000;
+        }
+    }
+
+    if (K <= stright_length)
+    {
+        cout << path.at(K) << endl;
+
+        return 0;
+    }
+
+    cout << path.at((K - stright_length) % loop_length + stright_length) << endl;
+
+    return 0;
+}
